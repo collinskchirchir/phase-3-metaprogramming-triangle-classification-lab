93c840f3b1a4f9a8ff0f2289fd5c53aced0a74d9
Collins Chirchir <collinskchirchir@gmail.com>
2023-02-19 14:09:35 +0300

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  # triange code
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c    
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    # Object for storing Boolean values 
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "you must provide valid values for triangle sides!"
+    end
+  end
 end
